2d8ac062b698efb40fe866f880d440cf97f75495
Tim Ziegel <tziegel@thrillworks.com>
2019-06-25 14:38:41 -0400

    Refactor site report and add accessibility audit functionality

---
+++ commands/accessibility.js
@@ -0,0 +1,40 @@
+const auditor = require('tw-accessibility-auditor');
+const asyncReduce = require('tw-async-reduce');
+const fs = require('fs-extra');
+const config = require('../config');
+
+module.exports = {
+  command: 'accessibility',
+  desc: 'Run an accessibility audit without running a full report. (Must have previously run a report).',
+  builder: argv => {
+    argv.option('verbose', {
+      describe: 'Turn on verbose logging.',
+      alias: 'v',
+      default: false,
+      type: 'boolean'
+    });
+  },
+  handler: async (argv = {}) => {
+    const summaryPath = `${config.reportPath}/summary.json`;
+    const summary = require(`../${summaryPath}`);
+    
+    if (!summary) console.log(`Could not run accessibility audit: site report not found. You must run a site report before the accessibility audit.`);
+    else {
+      if (argv.verbose) console.log(`Beginning accessibility audit`);
+      
+      const results = await asyncReduce(summary, async (page, index) => {
+        console.log(`Auditing page ${index+1} of ${summary.length}: ${page.url}`);
+        try {
+          const { url, violations } = await auditor.audit(page.url);
+          page.accessibilityAudit = { url, violations };
+          if (argv.verbose) console.log(`Found ${violations.length} violations.`);
+        } catch (e) {}
+        return page;
+      });
+      
+      await fs.writeFile(summaryPath, JSON.stringify(results, null, 2));
+      if (argv.verbose) console.log(`Accessibility audit complete`);
+    }
+  }
+};
+
---
+++ report-ui/src/components/accessibility-audit/index.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Box, Text, Paragraph, Image, Heading, Button } from 'grommet';
+import data from '../../report/lighthouse/summary.json';
+
+function Screenshot({ image, html }) {
+  return image && html && (
+    <Box pad="small" margin={{ bottom: 'medium', right: 'medium' }} align="center" key={image} border={{ color: 'silver' }} style={{ backgroundColor: 'whitesmoke' }}>
+      <Image src={`data:image/png;base64,${image}`} style={{ maxWidth: '100%' }} />
+      <Paragraph size="small" textAlign="center"><em>{html}</em></Paragraph>
+    </Box>
+  );
+}
+
+function Screenshots({ nodes }) {
+  return !nodes.length ? (<Paragraph>No screenshots available.</Paragraph>) : (
+    <Box align="start" direction="row" wrap="true" align="stretch">
+      {nodes.map(node => (
+        node.screenshots && node.screenshots.map(image => (<Screenshot image={image} html={node.html} />))
+      ))}
+    </Box>
+  );
+}
+
+function Violation({ violation }) {
+  const firstNode = violation.nodes.length && violation.nodes[0];
+  const firstAny = firstNode && firstNode.any.length && firstNode.any[0];
+  const allNodes = (firstAny && firstAny.relatedNodes.length) ? [...violation.nodes, ...firstAny.relatedNodes] : violation.nodes;
+  const uniqueNodes = allNodes.filter((node, index) => (
+    allNodes.map(node => node.html).indexOf(node.html) === index
+  ));
+
+  return (
+    <Box align="stretch" border={{ side: 'top', color: 'silver', size: 'xsmall' }}>
+      <Heading level="2">{violation.id}</Heading>
+      <Text>{violation.description}.</Text>
+      {firstNode && firstNode.failureSummary && (
+        <Box>
+          <Heading level="4">Recommendation</Heading>
+          <Text>{firstNode.failureSummary}.</Text>
+        </Box>
+      )}
+      <Heading level="4">Screenshots</Heading>
+      <Screenshots nodes={uniqueNodes} />
+    </Box>
+  );
+}
+
+function Violations({ violations }) {
+  return !violations || !violations.length ? (
+    <Paragraph>No violations found.</Paragraph>
+  ) : (
+    violations.map(violation => (<Violation violation={violation} key={violation.id} />))
+  );
+}
+
+export default function AccessibilityAudit({ match }) {
+  const name = match.params.name;
+  const result = data.find(datum => datum.name === name);
+  const audit = result && result.accessibilityAudit;
+  return (
+    <Box margin={{ vertical: 'small', horizontal: 'large' }}>
+      <Link to="/" style={{ color: 'gray' }}>&lsaquo; Back</Link>
+      {!audit ? (
+        <Paragraph>No violations found.</Paragraph>
+      ) : (
+        <Box>
+          <Heading color="dodgerblue" style={{ wordWrap: 'break-word' }}>{audit.url}</Heading>
+          <Violations violations={audit.violations} />
+          <Button primary={true} label={"Violations: " + audit.violations.length} style={{ position: 'fixed', bottom: '.5rem', right: '.5rem' }} />
+        </Box>
+      )}
+    </Box>
+  );
+}
+
