f41e2651c06c1d8cb87202c703497c5cbe6f4aff
Ramit Sanan <rsanan@Ramits-MacBook-Pro-2.local>
2019-11-08 16:58:53 -0500

    made more adjustments to generate_report script

--- generate_report.sh
+++ generate_report.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-cd .. && npm -i -g site-reporter lighthouse-batch
+cd ..
+sudo npm install -g site-reporter lighthouse-batch
 cd site-reporter
-echo "Enter URL of the website: "
-read url
-echo "You entered:" $url ". Do you want to continue (Y/N)? "
-read $url_answer
+url_answer="N"
+while [[ "$url_answer" != Y && "$url_answer" != y ]]
+do
+    echo "Enter URL of the website: "
+    read url
+    echo "You entered:" $url ". Is that correct(Y/N)?"
+    read url_answer
+done
+echo "Now executing site-reporter script $url"
 site-reporter report $url
+sudo npm uninstall -g site-reporter lighthouse-batch
