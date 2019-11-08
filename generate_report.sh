3fe1a9958f66ab3541f5133af6e0c4c5c72db053
Ramit Sanan <rsanan@Ramits-MacBook-Pro-2.local>
2019-11-08 13:16:10 -0500

    made changes to generate report script

--- generate_report.sh
+++ generate_report.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-npm i -g lighthouse-batch site-reporter
+cd .. && npm -i -g site-reporter lighthouse-batch
+cd site-reporter
 echo "Enter URL of the website: "
 read url
-echo "You entered:" $url;
+echo "You entered:" $url ". Do you want to continue (Y/N)? "
+read $url_answer
 site-reporter report $url
